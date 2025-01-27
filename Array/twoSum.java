d8592e84d8a8185ae0fd7a127fea38116485b20e
Soyam Kumar Gupta <120269736+Soyam2405@users.noreply.github.com>
2025-01-27 10:45:21 +0530

    Create twoSum.java

---
+++ Array/twoSum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       HashMap<Integer,Integer> map = new HashMap<>();
+       int res[] = new int[2];
+       for(int i=0;i<nums.length;i++){
+        map.put(nums[i],i);
+       }
+       for(int i=0;i<nums.length;i++){
+        if(map.containsKey(target-nums[i])){
+            if(i == map.get(target-nums[i])){
+                continue;
+            }
+            res[0] = i;
+            res[1] = map.get(target-nums[i]);
+        }
+       }
+       return res;
+    }  
+}
