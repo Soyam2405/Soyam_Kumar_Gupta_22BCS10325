5edbc7d518bc9c25c6f7366e0f86c197569b197b
Soyam Kumar Gupta <120269736+Soyam2405@users.noreply.github.com>
2025-01-27 10:55:09 +0530

    Update moveZeroes.java

--- Array/moveZeroes.java
+++ Array/moveZeroes.java
@@ -1,1 +1,16 @@
 //lt 283
+class Solution {
+    
+    public void moveZeroes(int[] nums) {
+
+    int j = 0;
+    for(int i = 0; i < nums.length; i++) {
+        if(nums[i] != 0)a {
+            int temp = nums[j];
+            nums[j] = nums[i];
+            nums[i] = temp;
+            j++;
+        }
+      }
+    }    
+}
